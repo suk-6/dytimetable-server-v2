3e5856ef839bc5fd5a105fd9ef3477cce0404470
suk-6 <me@suk.kr>
2024-06-03 15:13:31 +0900

    feat: Add RawDataModel interface for storing raw school data

---
+++ src/models/rawdata.ts
@@ -0,0 +1,43 @@
+export interface RawDataModel {
+    교사수: number;
+    자료446: string[];
+    학급수: number[];
+    요일별시수: number[][];
+    자료492: (number | string)[];
+    자료481: (((number[] | number)[] | number)[] | number)[];
+    전일제: number[];
+    버젼: string;
+    동시수업수: number;
+    담임: number[][];
+    가상학급수: number[];
+    특별실수: number;
+    열람제한일: string;
+    자료244: string;
+    학기시작일자: string;
+    학교명: string;
+    지역명: string;
+    학년도: number;
+    분리: number;
+    강의실: number;
+    시작일: string;
+    일과시간: string[];
+    일자자료: (number | string)[][];
+    오늘r: number;
+    자료147: (((number[] | number)[] | number)[] | number)[];
+    자료542: ((number[] | number)[] | number)[];
+    자료245: (
+        | (
+              | (number[] | number)[]
+              | ((number | string)[] | number[] | number)[]
+              | ((number | string)[] | number)[]
+              | number
+          )[]
+        | (
+              | (number[] | number)[]
+              | ((number | string)[] | number[] | number)[]
+              | number
+          )[]
+        | number
+    )[];
+    동시그룹: number[][];
+}
