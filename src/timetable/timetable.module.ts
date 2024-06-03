a8f6a1feb6fbae2d28c48e8cd2f3def13379a3df
suk-6 <me@suk.kr>
2024-06-03 21:13:37 +0900

    refactor: Add ScheduleModule and ParserModule to TimetableModule

--- src/timetable/timetable.module.ts
+++ src/timetable/timetable.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TimetableService } from './timetable.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ParserModule } from '../parser/parser.module';
 
 @Module({
+    imports: [ScheduleModule.forRoot(), ParserModule],
     providers: [TimetableService],
 })
 export class TimetableModule {}
