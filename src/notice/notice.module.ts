10c0d2e28d3c4d9382765f10e9c072a0157b3ed2
suk-6 <me@suk.kr>
2024-06-03 19:54:49 +0900

    feat: Add FirebaseModule and FirebaseService to NoticeModule

--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -3,12 +3,14 @@ import { NoticeService } from './notice.service';
 import { NoticeController } from './notice.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Notice, NoticeSchema } from './notice.schema';
+import { FirebaseModule } from '../firebase/firebase.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: Notice.name, schema: NoticeSchema },
         ]),
+        FirebaseModule,
     ],
     providers: [NoticeService],
     controllers: [NoticeController],
