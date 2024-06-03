d54287e89fd64fc18f437daa36bf3dc4d080737e
suk-6 <me@suk.kr>
2024-06-03 15:13:25 +0900

    chore: Add TimetableModule and ParserModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TimetableModule } from './timetable/timetable.module';
+import { ParserModule } from './parser/parser.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env`,
+        }),
+        ParserModule,
+        TimetableModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
