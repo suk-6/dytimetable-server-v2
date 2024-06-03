4b573544bcdbfc7587c845ec4f6d5dd085c09447
suk-6 <me@suk.kr>
2024-06-03 19:25:45 +0900

    feat: Add endpoints for homepage, installation, and password check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Redirect, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('main')
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get()
-    getHello(): string {
-        return this.appService.getHello();
+    @Redirect('https://github.com/suk-6/dytimetable', 301)
+    index() {}
+
+    @Get('install')
+    @Render('install')
+    install() {}
+
+    @Post('checkpassword')
+    async checkpassword(@Body('password') password: string) {
+        if (password === process.env.NOTICE_PASSWORD.toString()) {
+            return 'true';
+        } else {
+            return 'false';
+        }
     }
 }
