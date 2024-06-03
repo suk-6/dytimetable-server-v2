d33ad64b75cd21074cf7afc2485fda461f79e69e
suk-6 <me@suk.kr>
2024-06-03 19:25:24 +0900

    feat: Add @types/env.d.ts for environment variable typings

---
+++ @types/env.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        SCHOOL_CODE: number;
+        NEIS_API_KEY: string;
+        MONGO_ID: string;
+        MONGO_PASSWORD: string;
+        MONGO_HOST: string;
+        MONGO_PORT: number;
+        NOTICE_PASSWORD: number;
+    }
+}
