49f15479ce47c7f2d8407df304d4d42fdcb945d9
suk-6 <me@suk.kr>
2024-06-03 19:25:56 +0900

    feat: Add global prefix, API documentation, and view engine configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.setGlobalPrefix('v2', { exclude: ['/', '/install'] });
+
+    const config = new DocumentBuilder()
+        .setTitle('Dukyoung Timetable Service API Docs')
+        .setDescription(
+            'The Dukyoung Timetable Service API Docs for developers.',
+        )
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    app.setBaseViewsDir(join(__dirname, '..', 'static'));
+    app.setViewEngine('hbs');
+
     await app.listen(3000);
 }
 bootstrap();
