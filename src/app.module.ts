564c7ddc0772e6ff3e401722b0186bec5bea5a4f
suk-6 <me@suk.kr>
2024-06-03 19:25:48 +0900

    feat: Add NeisModule and NoticeModule for integrating NEIS API and managing notices

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { TimetableModule } from './timetable/timetable.module';
 import { ParserModule } from './parser/parser.module';
 import { ConfigModule } from '@nestjs/config';
+import { NeisModule } from './neis/neis.module';
+import { NoticeModule } from './notice/notice.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [
@@ -11,8 +14,18 @@ import { ConfigModule } from '@nestjs/config';
             isGlobal: true,
             envFilePath: `.env`,
         }),
+        MongooseModule.forRoot(
+            `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/admin`,
+            {
+                dbName: 'dytimetable',
+                user: process.env.MONGO_ID,
+                pass: process.env.MONGO_PASSWORD,
+            },
+        ),
         ParserModule,
         TimetableModule,
+        NeisModule,
+        NoticeModule,
     ],
     controllers: [AppController],
     providers: [AppService],
