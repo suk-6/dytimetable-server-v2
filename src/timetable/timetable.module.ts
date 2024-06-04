483dcc5aeb658c9309f535d8403519ac2cf3e875
suk-6 <me@suk.kr>
2024-06-04 10:13:51 +0900

    feat: Add TimetableController and TimetableService to TimetableModule

--- src/timetable/timetable.module.ts
+++ src/timetable/timetable.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TimetableService } from './timetable.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ParserModule } from '../parser/parser.module';
+import { TimetableController } from './timetable.controller';
 
 @Module({
     imports: [ScheduleModule.forRoot(), ParserModule],
     providers: [TimetableService],
+    controllers: [TimetableController],
 })
 export class TimetableModule {}
