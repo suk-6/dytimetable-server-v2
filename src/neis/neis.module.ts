3d6bb33c9ae9b7585060cc4d895a11d491567098
suk-6 <me@suk.kr>
2024-06-10 00:25:33 +0900

    feat: Add ScheduleModule and FirebaseModule to NeisModule imports

--- src/neis/neis.module.ts
+++ src/neis/neis.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NeisService } from './neis.service';
 import { NeisController } from './neis.controller';
+import { ScheduleModule } from '@nestjs/schedule';
+import { FirebaseModule } from '../firebase/firebase.module';
 
 @Module({
+    imports: [ScheduleModule.forRoot(), FirebaseModule],
     providers: [NeisService],
     controllers: [NeisController],
 })
